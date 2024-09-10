36acd957cded06a68d9551c2d0a32841744a5143
Shrinidhi-anand <shrinidhia1@gmail.com>
2024-09-10 19:15:55 +0530

    Design file for 32x8 RAM
    Here is the design file of 32x8 RAM.

---
+++ SV Environment for 32x8 RAM/ram.sv
@@ -0,0 +1,31 @@
+//***********************DESIGN*******************//
+module ram ( clk,rst,data_in,rd_en,wr_en,addr,data_out);
+  
+  input clk,rst,rd_en,wr_en;
+  input [DATA_WIDTH:0] data_in;
+  input [ADDR_WIDTH:0]addr;
+  output logic [DATA_WIDTH:0]data_out;
+  integer i;
+  logic [DATA_WIDTH:0]mem[MEM_SIZE:0];
+  
+  always@(posedge clk)
+    begin
+      if(rst)
+        begin
+          for(i=0;i<=MEM_SIZE;i=i+1)
+            mem[i] <= 8'dx;
+        end
+      else if(wr_en == 1)
+        mem[addr] <= data_in;
+    end
+  
+  always@(posedge clk)
+    begin
+      if(rst)
+        data_out <= 8'dx;
+      else if(rd_en == 1)
+        data_out <= mem[addr];
+      else
+        data_out <= 8'dx;
+    end
+endmodule
